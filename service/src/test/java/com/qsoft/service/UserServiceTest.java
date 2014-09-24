663a0c6a25eb532d9838b5053af7ed6a009b73d2
Truong Quy Quynh <quynhtq@qsoft.com.vn>
2014-09-24 11:58:49 +0700

    add test

---
+++ service/src/test/java/com/qsoft/service/UserServiceTest.java
@@ -0,0 +1,17 @@
+package com.qsoft.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by quynhtq on 9/24/14.
+ */
+public class UserServiceTest
+{
+    @Test
+    public void test()
+    {
+        assertEquals(1,1);
+    }
+}
